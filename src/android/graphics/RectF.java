ec71aca2f36733578f6cbd3355065977c9cc74a2
Almos Rajnai <almos.rajnai@gmail.com>
2015-03-22 12:05:11 +1300

    Fixed union calculation in RectF

--- src/android/graphics/RectF.java
+++ src/android/graphics/RectF.java
@@ -53,15 +53,21 @@ public class RectF implements AndroidClass {
     }
 
     public void union(RectF bounds) {
-        OutputBuilder.appendMethodCall(this, "union", "%s", bounds.getInstanceName());
+        union(bounds.left, bounds.top, bounds.right, bounds.bottom);
+    }
 
+    public void union(float left, float top, float right, float bottom) {
         //Direct copy from Android sources: http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.0.2_r1/android/graphics/Rect.java#Rect.union%28int%2Cint%2Cint%2Cint%29
         if ((left < right) && (top < bottom)) {
             if ((this.left < this.right) && (this.top < this.bottom)) {
-                if (this.left > left) this.left = left;
-                if (this.top > top) this.top = top;
-                if (this.right < right) this.right = right;
-                if (this.bottom < bottom) this.bottom = bottom;
+                if (this.left > left)
+                    this.left = left;
+                if (this.top > top)
+                    this.top = top;
+                if (this.right < right)
+                    this.right = right;
+                if (this.bottom < bottom)
+                    this.bottom = bottom;
             } else {
                 this.left = left;
                 this.top = top;
