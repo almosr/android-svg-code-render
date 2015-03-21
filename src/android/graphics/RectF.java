abe1df0d6df60e466708e3e27a951d9c0867c543
Almos Rajnai <almos.rajnai@gmail.com>
2015-03-21 19:04:58 +1300

    RectF is initialized properly and methods are emitting code, union implementation was copied from Android sources.

--- src/android/graphics/RectF.java
+++ src/android/graphics/RectF.java
@@ -2,6 +2,7 @@ package android.graphics;
 
 import android_svg_code_render.AndroidClass;
 import android_svg_code_render.Initializer;
+import android_svg_code_render.OutputBuilder;
 
 /**
  * Created by racs on 2015.03.17..
@@ -15,12 +16,8 @@ public class RectF implements AndroidClass {
     private String mInstanceName;
 
     public RectF(float left, float top, float right, float bottom) {
-        this();
-
-        this.left = left;
-        this.right = right;
-        this.top = top;
-        this.bottom = bottom;
+        Initializer.init(this, String.format("%ff, %ff, %ff, %ff", left, top, right, bottom));
+        initFields(left, top, right, bottom);
     }
 
     public RectF() {
@@ -28,7 +25,15 @@ public class RectF implements AndroidClass {
     }
 
     public RectF(Rect rect) {
-        this(rect.left, rect.top, rect.right, rect.bottom);
+        Initializer.init(this, String.format("%s", rect.getInstanceName()));
+        initFields(rect.left, rect.top, rect.right, rect.bottom);
+    }
+
+    private void initFields(float left, float top, float right, float bottom) {
+        this.left = left;
+        this.right = right;
+        this.top = top;
+        this.bottom = bottom;
     }
 
     public float width() {
@@ -40,11 +45,30 @@ public class RectF implements AndroidClass {
     }
 
     public void offset(float x, float y) {
-        throw new RuntimeException("Dummy function");
+        OutputBuilder.appendMethodCall(this, "offset", "%ff, %ff", x, y);
+        left += x;
+        right += x;
+        top += y;
+        bottom += y;
     }
 
     public void union(RectF bounds) {
-        throw new RuntimeException("Dummy function");
+        OutputBuilder.appendMethodCall(this, "union", "%s", bounds.getInstanceName());
+
+        //Direct copy from Android sources: http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android/5.0.2_r1/android/graphics/Rect.java#Rect.union%28int%2Cint%2Cint%2Cint%29
+        if ((left < right) && (top < bottom)) {
+            if ((this.left < this.right) && (this.top < this.bottom)) {
+                if (this.left > left) this.left = left;
+                if (this.top > top) this.top = top;
+                if (this.right < right) this.right = right;
+                if (this.bottom < bottom) this.bottom = bottom;
+            } else {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+        }
     }
 
     @Override
