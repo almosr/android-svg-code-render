8037b3b4fafd0559e9f44242b8dca0344c4f2382
Almos Rajnai <almos.rajnai@gmail.com>
2015-04-12 15:57:00 +1200

    Version bumped to v1.0.1 after release

--- src/android_svg_code_render/Version.java
+++ src/android_svg_code_render/Version.java
@@ -16,7 +16,7 @@ import java.util.zip.ZipEntry;
 public class Version {
     public static final int MAJOR = 1;
     public static final int MINOR = 0;
-    public static final int REVISION = 0;
+    public static final int REVISION = 1;
 
     public static final String FULL = String.format("%d.%d.%d", MAJOR, MINOR, REVISION);
 
