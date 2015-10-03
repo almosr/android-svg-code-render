afe29ac6473f7c5c6dc8254ed26bc996e0701198
Almos Rajnai <almos.rajnai@gmail.com>
2015-10-04 11:13:30 +1300

    Implemented DashPathEffect class (resolves #61)

--- src/android/graphics/DashPathEffect.java
+++ src/android/graphics/DashPathEffect.java
@@ -1,10 +1,19 @@
 package android.graphics;
 
+import android.util.FloatConstantArray;
+import android_svg_code_render.OutputBuilder;
+
 /**
  * Created by racs on 2015.03.17..
  */
 public class DashPathEffect extends PathEffect {
     public DashPathEffect(float[] intervals, float offset) {
-        throw new RuntimeException("Dummy function");
+        OutputBuilder.addImport(Shader.class);
+
+        FloatConstantArray intervalsConst = new FloatConstantArray(intervals);
+
+        init(OutputBuilder.dependencyList(intervalsConst), "%s, %ff",
+                intervalsConst.getInstanceName(this),
+                offset);
     }
 }
--- src/android/graphics/PathEffect.java
+++ src/android/graphics/PathEffect.java
@@ -1,7 +1,9 @@
 package android.graphics;
 
+import android_svg_code_render.AndroidClass;
+
 /**
  * Created by racs on 2015.03.17..
  */
-public class PathEffect {
+public abstract class PathEffect extends AndroidClass {
 }
