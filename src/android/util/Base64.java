8493e5c3aeaa91c5318e279925820e10f5aa479b
Almos Rajnai <almos.rajnai@gmail.com>
2015-10-04 10:36:39 +1300

    Base64-encoded images are decoded now

--- src/android/util/Base64.java
+++ src/android/util/Base64.java
@@ -4,9 +4,10 @@ package android.util;
  * Created by racs on 2015.03.17..
  */
 public class Base64 {
+    // This flag is ignored, required only for source level compatibility
     public static final int DEFAULT = 0;
 
     public static byte[] decode(String base64string, int flags) {
-        throw new RuntimeException("Dummy function");
+        return java.util.Base64.getDecoder().decode(base64string);
     }
 }
