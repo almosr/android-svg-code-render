7f10b4ee81b79e8eb4772df049da144bce5d5a93
Almos Rajnai <almos.rajnai@appmindful.com>
2016-09-03 12:04:42 +1200

    Changed maximum lines in a render method to a lower number to avoid generating too many instructions in a method which will not be processed by ART then. (Fixes #72)

--- src/android_svg_code_render/OutputBuilder.java
+++ src/android_svg_code_render/OutputBuilder.java
@@ -8,7 +8,7 @@ import java.util.*;
  * @author Almos Rajnai
  */
 public class OutputBuilder {
-    private static final int METHOD_SIZE_THRESHOLD = 3000;
+    private static final int METHOD_SIZE_THRESHOLD = 1500;
     private static float sWidth;
     private static float sHeight;
     private static ArrayList<OutputItem> sStaticItems;
